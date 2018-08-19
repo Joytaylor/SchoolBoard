f59d44a58874b50f2dafbf4f3ae4463eecca89dc
Joy <jtaylor2@imsa.edu>
2018-08-19 13:45:54 -0500

    new file creation

---
+++ createNewFile.js
@@ -0,0 +1,17 @@
+
+var fs = require('fs')
+var courseName = 'replacement';
+var teachers = 'dr.coolness';
+fs.readFile("base.html", 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var data1 =data.split('replacement').join(courseName);
+  var data2 =
+   data1.split('dr.coolness').join(teachers);
+  var result = data2.split('replacementQuestionPage.html') .join(courseName+'QuestionPage.html');
+
+  fs.writeFile(courseName + ".html", result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
