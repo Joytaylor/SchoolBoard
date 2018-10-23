038b599bc511bf967e785f4e53d189c57e93db9e
Sam Anozie <sanoziec@gmail.com>
2018-10-23 11:38:04 -0500

    Changed styles to stats.php

--- Stats.php
+++ Stats.php
@@ -7,20 +7,13 @@
 <meta charset = "UTF-8"/>
 <link rel = "stylesheet" type = "text/css" href = "styles.css"/>
 <link rel = "icon" href = "favicon.jpg" type = "image/jpg"/>
-<!--Fonts-->
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative" rel="stylesheet">
-
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <style>
-@import url('https://fonts.googleapis.com/css?family=Pontano+Sans');
+
 html {
 	height: 100%;
 }
@@ -30,6 +23,7 @@ body {
 	background-image: url('backpic.jpg');
 	background-size: cover;
 	overflow: auto;
+	margin: auto;
 }
 </style>
 </head>
@@ -39,16 +33,16 @@ body {
 		<img src = "Picture1.png" id = "Stats"/>
 	</a>
 </div>
-<div class = "body" id = "Stats">
 <div class = "classname" id = "Stats">
 	<div class = "heading">
 		<h1>Stats</h1>
 	</div>
 	<div class = "statement">
-		<p> Teachers - Trainor</p>
+		<p>Q u e s t i o n s</p>
 	</div>
 </div>
 <div class = "outerContainer">
+<div class = "innerContainter">
 	<?php
 			include("config.php");
 			mysqli_select_db($conn, 'SchoolBoard');
@@ -56,19 +50,16 @@ body {
 			$result = $conn->query($sql) or die($conn->error);
 
 			if (true) {
-				echo " <h3> PAST ANSWERS </h3>";
+				echo " <h3>This Week</h3><br><div class = 'strip'></div>";
 				while($row = $result->fetch_assoc()) {
-					echo "<h6> ". $row['question']."</h6><br/>";
+					echo "<div class = 'question'><h6> ". $row['question']."</h6></div><br/>";
 				}
+			//Each question will be a div, so when data is pulled from the database, will it only pull the text  for the question, then put it in the div, or will it pull the whole div? in any case, I will put the div below with the content needed, including space for the voting section and the counter for the votes. I will assume that the text will be pulled, so I will leave an empty <p> tag for pastes.
 			}
 	 ?>
-
-
-		<h2>No more questions asked for this class yet, check again soon!</h2>
 		<div class = "ask"><a href = "StatsQuestionPage.html"><h3>Ask a question</h3></a></div>
 		<h4><a class = "backLink" href = "SchoolBoardAccountPage.html">Go back to account</a></h4>
-    </div>
-</div>
+   </div>
 </div>
 </body>
 </html>
