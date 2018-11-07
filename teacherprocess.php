ae9c555be850ef0930035043b7226d621e2af484
Joy <jtaylor2@imsa.edu>
2018-11-07 12:13:45 -0600

    error went away now the info is not passing

--- teacherprocess.php
+++ teacherprocess.php
@@ -4,9 +4,10 @@ include("cookiecheck.php");
 
  $question = $_POST['question'];
  $subject = $_POST['subject'];
- $sql = " UPDATE Question SET teacherResponce = $_POST['teacherResponce']
-  WHERE subject = $_POST['subject'] AND question = $_POST['Question']" ;
-
+ $teacher = $_POST['teacherResponce'];
+ echo $teacher;
+ $sql = "UPDATE Question SET teacherResponce = "." $_POST[teacherResponce] "." WHERE subject= "."$_POST[subject]"." AND question = "."$_POST[question]";
+echo $sql;
  if (mysqli_query($conn, $sql)){
    header( 'Location: /SchoolBoard/'.$subject.".php");
  }
